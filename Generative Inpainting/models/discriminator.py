4cbd272650c28fa0d0d60570816a31b03da5e3f0
AshishNair911 <166924732+AshishNair911@users.noreply.github.com>
2025-07-07 16:04:36 +0530

    Create discriminator.py

---
+++ Generative Inpainting/models/discriminator.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, input_channels=3):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(input_channels, 64, 4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, 4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 256, 4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 512, 4, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(512, 1, 4, stride=1, padding=1)
+        )
+
+    def forward(self, x):
+        return self.model(x)
