ebae0e17fd39ff7f1241be27843c2c644b07a036
AshishNair911 <166924732+AshishNair911@users.noreply.github.com>
2025-07-07 14:30:17 +0530

    Update discriminator.py

--- Generative Impainting Model/model/discriminator.py
+++ Generative Impainting Model/model/discriminator.py
@@ -1,8 +1,9 @@
+import torch
 import torch.nn as nn
 
 class PatchDiscriminator(nn.Module):
     def __init__(self, in_channels=3):
-        super().__init__()
+        super(PatchDiscriminator, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(in_channels, 64, 4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
